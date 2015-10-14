5cdfa437d2dc07ee6698a0ac9871de047d82c3f2
ackuser <mail@ackarim.com>
2015-10-14 21:10:03 +0100

    adding scope variables

--- README.md
+++ README.md
@@ -5,3 +5,17 @@ http://tutorials.jenkov.com/svg/scripting.html
 
 
 http://stackoverflow.com/questions/23655009/how-to-set-the-id-attribute-of-a-html-element-dynamically-with-angular-js
+
+https://css-tricks.com/guide-svg-animations-smil/
+
+http://tutorials.jenkov.com/angularjs/watch-digest-apply.html
+
+http://stackoverflow.com/questions/8455773/svg-trigger-animation-with-event
+
+http://stackoverflow.com/questions/26614750/smil-animation-on-svg-ng-repeated-element-only-occurs-on-page-load
+
+http://stackoverflow.com/questions/14964049/inline-svg-and-jquery-selection
+
+http://www.heapoverflow.me/question-animating-svg-path-when-changing-value-27279225
+
+http://jsfiddle.net/fg9e7eo4/1/
--- app.js
+++ app.js
@@ -2,25 +2,33 @@ angular.module('app', [])
 .controller('mainController', ["$scope", function($scope){
 
 
-	$scope.init = function(percentage, duration, repetition)
+	$scope.init = function(percentage, duration, repetition, returning)
 	{
-		$scope.duration = duration;
+		$scope.duration = duration + "s";
 		$scope.completionPercent = percentage;
 		$scope.repeat = repetition;
 		$scope.percent = percentage + "%";
+		$scope.return = returning;
+		$scope.interval = "0;" + $scope.completionPercent/100+ ";";
+
 	};
 
 
 	$scope.submit = function() {
-		alert($scope.completionPercent + " " + $scope.repeat + " " + $scope.duration);
+		alert($scope.completionPercent + " " + $scope.repeat + " " +
+		$scope.duration + " " + $scope.return);
 	};
 
-	$scope.$watch(function() {
-		return $scope.repeat;
-	}, function(newValue, oldValue) {
-		console.log("change detected: " + newValue)
+	$scope.$watch('return', function(newValue, oldValue) {
+		console.log(newValue);
+		if (newValue === 'false')
+			$scope.interval = "0;" + $scope.completionPercent/100+ ";";
+		else
+			$scope.interval = "0;" + $scope.completionPercent/100+ ";0;";
+		console.log($scope.interval);
+		angular.element($('#animation1')[0]).beginElement();
+		angular.element($('#animation2')[0]).beginElement();
 	});
-
 
 
 }]);
