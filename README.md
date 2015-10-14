5f7453024bda6695900384a7ce44b966bf0f5edc
ackuser <mail@ackarim.com>
2015-10-14 20:09:14 +0100

    angular controller

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # angular-circle-svg-progress-bar
+
+
+http://tutorials.jenkov.com/svg/scripting.html
+
+
+http://stackoverflow.com/questions/23655009/how-to-set-the-id-attribute-of-a-html-element-dynamically-with-angular-js
--- app.js
+++ app.js
@@ -1,4 +1,26 @@
 angular.module('app', [])
-	.controller('mainController', ["$scope", function($scope){
- 
-	}]);
+.controller('mainController', ["$scope", function($scope){
+
+
+	$scope.init = function(percentage, duration, repetition)
+	{
+		$scope.duration = duration;
+		$scope.completionPercent = percentage;
+		$scope.repeat = repetition;
+		$scope.percent = percentage + "%";
+	};
+
+
+	$scope.submit = function() {
+		alert($scope.completionPercent + " " + $scope.repeat + " " + $scope.duration);
+	};
+
+	$scope.$watch(function() {
+		return $scope.repeat;
+	}, function(newValue, oldValue) {
+		console.log("change detected: " + newValue)
+	});
+
+
+
+}]);
