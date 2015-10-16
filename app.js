5a66d90d1db38e3d3e9bcb311b7c97937422104d
ackuser <mail@ackarim.com>
2015-10-16 09:43:19 +0100

    First Stable Release

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ angular.module('app', [])
 
 	$scope.graph= {};
 
-	//console.log($scope.graph);
 	$scope.init2 = function(percentage, duration, repetition, returning)
 	{
 		var percent = percentage / 100;
@@ -28,51 +27,36 @@ angular.module('app', [])
 }])	.directive('pieChart', function() {
 	return {
 		restrict: 'E',
-		//template: html,
-		//controller: 'testCtrl',
 		templateUrl: 'circle.html',
 		scope: {
 			duration: '=',
 			percentage: '=',
 			repeat: '=',
-			returning: '=',
-			interval: '='
+			returning: '='
 		},
 
 		link: function(scope, elem, attrs) {
 
 
 
-			/*scope.$watch('returning', function(newValue, oldValue) {
-			scope.interval = '0;34;0;';
-			scope.repeat = 'indefinite';
-			scope.duration = '3s';
-			scope.completionPercent = '74';
-		});*/
-		//	*/
-
-		scope.$watch(function(){
-			console.log(scope.duration);
-			console.log(scope.percentage);
-			scope.interval = "0;" + scope.percentage / 100 + ";0;"
-			if(scope.returning === 'false'){
-				//scope.graph.interval = "0;" + scope.graph.completionPercent/100+ ";";
-				//console.log(scope.graph.interval);
-			}
-			else{
-				//scope.graph.interval = "0;" + scope.graph.completionPercent/100+ ";0;";
-				//console.log(scope.graph.completionPercent);
+			scope.$watch(function(){
+				console.log(scope.returning);
+				console.log(scope.duration);
+				console.log(scope.percentage);
+				if(scope.returning === 'true'){
+					scope.interval = "0;" + scope.percentage / 100 + ";"
+				}
+				else{
+					scope.interval = "0;" + scope.percentage / 100 + ";0;"
+				}
 				console.log(scope.interval);
+				animateIt();
 			}
-			angular.element( document.querySelector( '#animation1' ) )[0].beginElement();
-			angular.element( document.querySelector( '#animation2' ) )[0].beginElement();
-		},
-		animateIt
 		);
 
 		function animateIt(){
-			//angular.element( document.querySelector( '#animation1' ) )[0].beginElement();
-			//angular.element( document.querySelector( '#animation2' ) )[0].beginElement();
+			angular.element( document.querySelector( '#animation1' ) )[0].beginElement();
+			angular.element( document.querySelector( '#animation2' ) )[0].beginElement();
 		}
 
 	}
